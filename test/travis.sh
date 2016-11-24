813d8bfabcf33761cd13146af8f5d23b08871392
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-11-24 22:16:24 +0300

    Create travis.sh

---
+++ test/travis.sh
@@ -0,0 +1,24 @@
+export DOMAIN=${TRAVIS_BRANCH::${#TRAVIS_BRANCH}-5}
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+echo -e "machine github.com\nlogin $GITHUB_NAME\npassword $GITHUB_TOKEN" >> ~/.netrc
+mkdir ~/.config || true
+echo -e "---\ngithub.com:\n- oauth_token: $GITHUB_TOKEN\n  user: $GITHUB_NAME" >> ~/.config/hub
+
+wget https://github.com/galeksandrp/https-everywhere/archive/check-sublist3r.tar.gz -O - | tar xz
+mv https-everywhere-check-sublist3r ~/workspace
+wget https://github.com/aboul3la/Sublist3r/archive/master.tar.gz -O - | tar xz
+mv Sublist3r-master ~/workspace/Sublist3r
+git remote add upstream https://github.com/EFForg/https-everywhere.git
+git fetch upstream master
+git checkout -b master upstream/master
+wget https://github.com/github/hub/releases/download/v2.2.8/hub-linux-amd64-2.2.8.tgz -O - | tar xz --strip=1 -C ~ hub-linux-amd64-2.2.8
+export PATH=~/bin:$PATH
+chmod +x ~/workspace/Sublist3r/sublist3r.py ~/workspace/*.sh
+
+cd src/chrome/content/rules
+~/workspace/generate.sh $DOMAIN
+git add $TRAVIS_BUILD_DIR/src/chrome/content/rules/$DOMAIN.xml
+git commit -m $DOMAIN
+git push -u origin $DOMAIN
+hub pull-request -h $GITHUB_OWNER:$DOMAIN -m $DOMAIN
