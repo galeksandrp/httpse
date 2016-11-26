8e9530839cc82cc8dc3ac678f89389ddbc5a6224
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-11-26 16:00:37 +0000

    Add shallow clone

--- test/travis.sh
+++ test/travis.sh
@@ -15,8 +15,8 @@ wget https://github.com/aboul3la/Sublist3r/archive/master.tar.gz -O - | tar xz
 mv Sublist3r-master ~/workspace/Sublist3r
 git remote add upstream https://github.com/EFForg/https-everywhere.git
 git remote add downstream https://github.com/$GITHUB_NAME/https-everywhere.git
-git fetch upstream master
-git fetch downstream $DOMAIN || true
+git fetch --depth=50 upstream master
+git fetch --depth=50 downstream $DOMAIN || true
 git checkout -b $DOMAIN downstream/$DOMAIN && git merge upstream/master --ff-only -q || git checkout -b $DOMAIN upstream/master
 wget https://github.com/github/hub/releases/download/v2.2.8/hub-linux-amd64-2.2.8.tgz -O - | tar xz --strip=1 -C ~ hub-linux-amd64-2.2.8
 chmod +x ~/workspace/Sublist3r/sublist3r.py ~/workspace/*.sh
